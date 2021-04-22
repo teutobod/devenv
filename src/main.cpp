534f46d87db16af9a29fa1476600f407160d5087
David Hurtado <extern.david.hurtado@tttech.com>
2021-04-22 09:23:36 +0000

    Add RomanVequals5 test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
 #include <cstring>
 
-size_t RomanToInt(const char *string) { return strlen(string); }
+size_t RomanToInt(const char *string) {
+  size_t integer = 0;
+  char romanLetter = string[0];
+  switch (romanLetter) {
+    case 'I':
+      integer = 1;
+      break;
+    case 'V':
+      integer = 5;
+      break;
+  }
+  return integer;
+}
--- src/test_main.cpp
+++ src/test_main.cpp
@@ -3,3 +3,4 @@
 #include "main.h"
 
 TEST(Maintest, RomanIequal1) { EXPECT_EQ(1, RomanToInt("I")); }
+TEST(Maintest, RomanVequal5) { EXPECT_EQ(5, RomanToInt("V")); }
