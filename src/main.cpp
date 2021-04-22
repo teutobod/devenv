cdebac4de57b5dc244e70556691486408df551f9
bittl <johannes.bittl@tttech-auto.com>
2021-04-22 09:53:18 +0000

    next commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <cstring>
 
-size_t RomanToInt(const char *string) {
-  size_t integer = 0;
-  char romanLetter = string[0];
+static size_t ConvertRomanLetterToInteger(const char romanLetter) {
   switch (romanLetter) {
     case 'I':
-      integer = 1;
-      break;
+      return 1;
     case 'V':
-      integer = 5;
-      break;
+      return 5;
+  }
+}
+
+static size_t ForEveryLetterInRomanString(const char *string) {
+  int length = strlen(string);
+  size_t integer = 0;
+
+  for (int i = 0; i < length; i++) {
+    integer = integer + ConvertRomanLetterToInteger(string[i]);
   }
   return integer;
+}
+
+size_t RomanToInt(const char *string) {
+  return ForEveryLetterInRomanString(string);
 }
--- src/test_main.cpp
+++ src/test_main.cpp
@@ -4,3 +4,4 @@
 
 TEST(Maintest, RomanIequal1) { EXPECT_EQ(1, RomanToInt("I")); }
 TEST(Maintest, RomanVequal5) { EXPECT_EQ(5, RomanToInt("V")); }
+TEST(Maintest, RomanIIequal2) { EXPECT_EQ(2, RomanToInt("II")); }
